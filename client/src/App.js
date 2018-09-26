66b64dd39371f8e97d4377d19cfb52848f34986a
Tommie Behrenbeck <behrenbt@gmail.com>
2018-09-26 14:02:19 -0500

    clean up src

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
 // import Navbar from "./components/Navbar/Navbar";
 // import Newsfeed from "./pages/Newsfeed/Newsfeed";
 import Profile from "./pages/Profile/Profile";
-
-
 
 const App = () => (
   <Router>
     <div>
       {/* <Navbar /> */}
-        <Route exact path="/" />
-        {/* <Route exact path="/newsfeed" component={Newsfeed} /> */}
-        <Route exact path="/profile" component={Profile} />
+      <Route exact path="/" />
+      {/* <Route exact path="/newsfeed" component={Newsfeed} /> */}
+      <Route exact path="/profile" component={Profile} />
     </div>
   </Router>
 );
-
 
 export default App;
