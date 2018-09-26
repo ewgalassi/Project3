8918c27d358a5850e5fd325f14f6e1fe3e48d5be
mostar <mominahmad93@gmail.com>
2018-09-26 14:03:41 -0500

    added client API routes

---
+++ client/src/utils/postAPI.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export default  {
+    // get all the posts
+    getPosts: function () {
+        return axios.get("/api/posts");
+    },
+
+    savePost: function (postData) {
+        return axios.post("/api/posts", postData)
+    },
+
+    likePost: function () {
+        return axios.put("/api/posts")
+    },
+    commentPost: function (id) {
+        return axios.post("/api/posts/comment")
+    },
+    // Gets the post with the given id
+    getPost: function (id) {
+        return axios.get("/api/posts/" + id);
+    },
+
+    editPost: function(id){
+        return axios.put("/api/posts/" + id)
+    },
+    // Deletes the post with the given id (only if you made it)
+    deletePost: function (id) {
+        return axios.delete("/api/posts/" + id);
+    },
+
+
+    // like a post
+
+
+   
+
+
+}
