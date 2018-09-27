2a023bbfdeade4f1b5dd519e3f4c070383fc57b7
Adnan Yousef <yousef.eddy@gmail.com>
2018-09-27 13:18:52 -0500

    Added login and register page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import UserAPI from "../../utils/userAPI";
 
 //components needed:
 //navbar
@@ -11,15 +12,46 @@ class Login extends Component {
     };
 
     handleInput = event => {
-        
-    }
-    render(){
-        return(
-            <h3>login page</h3>
-        )
-     
-    }
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
+    handleSubmit = event => {
+        event.preventDefault();
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        };
+        UserAPI.loginUser(user).then(data => {
+            console.log(data);
+            window.location.replace("/");
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+
+                    <label>Username</label>
+                    <input name="username" value={this.state.username} onChange={this.handleInput} />
+
+                    <br />
+
+                    <label>Password</label>
+                    <input name="password" value={this.state.password} onChange={this.handleInput} type="password" />
+
+                    <br />
+
+                    <button type="submit" onClick={this.handleSubmit}>Login</button>
+                </form>
+
+                <p>{this.state.message}</p>
+            </div>
+        )
+    }
 }
 
 export default Login;
