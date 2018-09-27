51195c79b27991a6042a002d4de69dc725aafb0e
Adnan Yousef <yousef.eddy@gmail.com>
2018-09-27 09:47:24 -0500

    Added yarn.lock to gitignore

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+// const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require("bcrypt");
 mongoose.promise = Promise;
 
 // Define userSchema
