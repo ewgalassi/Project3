82f06053a26987764ed6846aab72ade28e8870da
Adnan Yousef <yousef.eddy@gmail.com>
2018-10-01 14:47:23 -0500

    Made delete button

--- client/src/components/Postfeed/PostHeader/PostHeader.js
+++ client/src/components/Postfeed/PostHeader/PostHeader.js
@@ -1,8 +1,22 @@
 import React from "react";
 import PicIcon from "../../NewPost/PicIcon/PicIcon";
 import "./PostHeader.css";
+import PostAPI from "../../../utils/postAPI";
 
 class PostHeader extends React.Component {
+
+  handleDelete = (id) => {
+    console.log(id);
+    PostAPI.deletePost(id).then(data => {
+      if (data.data.success) {
+        window.location.reload();
+      } else {
+        console.log(data.data);
+      };
+    }).catch(err => {
+      console.log(err);
+    });
+  };
 
   render() {
     return (
@@ -11,6 +25,10 @@ class PostHeader extends React.Component {
         <a href={"/profile/" + this.props.authorId}>
           <h5 className="author">{this.props.author}</h5>
         </a>
+        <button 
+        className="btn btn-sm btn-danger float-right"
+        onClick={() => this.handleDelete(this.props.id)}
+        >X</button>
       </div>
     )
   };
