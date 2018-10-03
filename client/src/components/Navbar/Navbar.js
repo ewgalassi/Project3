8b020b2f90c89fecac256618ccca2442c956f58e
Tommie Behrenbeck <behrenbt@gmail.com>
2018-10-03 12:57:41 -0500

    search box

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -10,6 +10,18 @@ const Navbar = () => (
       <Link id="accountLogo" className="navbar-brand" to="/">
         Project 3
       </Link>
+      <form className="form-inline my-2 my-lg-0">
+        <input
+          id="searchbox"
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
       <div>
         <ul className="navbar-nav">
           <li className="nav-item">
