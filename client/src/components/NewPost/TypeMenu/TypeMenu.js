9381f126a1820b44d0ecbecf92972a5a2b8c7943
Brittani <brittani1128@gmail.com>
2018-10-04 12:51:58 -0500

    save and copy snippet icons

--- client/src/components/NewPost/TypeMenu/TypeMenu.js
+++ client/src/components/NewPost/TypeMenu/TypeMenu.js
@@ -1,42 +1,42 @@
-import React, { Component } from "react";
-import "./TypeMenu.css";
+// import React, { Component } from "react";
+// import "./TypeMenu.css";
 
-// import TypeBtn from "./TypeBtn";
+// // import TypeBtn from "./TypeBtn";
 
-class TypeMenu extends Component {
-  state = {
-    isOpen: false
-  };
+// class TypeMenu extends Component {
+//   state = {
+//     isOpen: false
+//   };
 
-  toggleOpen = () => this.setState({ isOpen: !this.state.isOpen });
+//   toggleOpen = () => this.setState({ isOpen: !this.state.isOpen });
 
-  render() {
-    const menuClass = `dropdown-menu${this.state.isOpen ? " show" : ""}`;
-    return (
-      <div className="dropdown" onClick={this.toggleOpen}>
-        <button
-          className="btn btn-secondary dropdown-toggle"
-          type="button"
-          id="dropdownMenuButton"
-          data-toggle="dropdown"
-          aria-haspopup="true"
-        >
-          Select Type
-        </button>
-        <div className={menuClass} aria-labelledby="dropdownMenuButton">
-          <a className="dropdown-item" >
-            Status
-          </a>
-          <a className="dropdown-item" >
-            Snippet
-          </a>
-          <a className="dropdown-item" >
-            Article
-          </a>
-        </div>
-      </div>
-    );
-  }
-}
+//   render() {
+//     const menuClass = `dropdown-menu${this.state.isOpen ? " show" : ""}`;
+//     return (
+//       <div className="dropdown" onClick={this.toggleOpen}>
+//         <button
+//           className="btn btn-secondary dropdown-toggle"
+//           type="button"
+//           id="dropdownMenuButton"
+//           data-toggle="dropdown"
+//           aria-haspopup="true"
+//         >
+//           Select Type
+//         </button>
+//         <div className={menuClass} aria-labelledby="dropdownMenuButton">
+//           <a className="dropdown-item" >
+//             Status
+//           </a>
+//           <a className="dropdown-item" >
+//             Snippet
+//           </a>
+//           <a className="dropdown-item" >
+//             Article
+//           </a>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default TypeMenu;
+// export default TypeMenu;
